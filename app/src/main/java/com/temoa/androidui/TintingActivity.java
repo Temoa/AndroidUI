75ec9e24d1993063b92cb9961a45974cfcfed6c3
Temoa <temoa_mic@hotmail.com>
2016-09-15 16:51:04 +0800

    update

---
+++ app/src/main/java/com/temoa/androidui/TintingActivity.java
@@ -0,0 +1,13 @@
+package com.temoa.androidui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TintingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tinting);
+    }
+}
